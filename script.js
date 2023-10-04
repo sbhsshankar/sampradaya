ab72f74d3bb5d8c74e5a6f95fffc59328f79a43a
Sbhss <shankarpulaparthi1216@gmail.com>
2023-10-04 08:41:01 +0530

    done

---
+++ script.js
@@ -0,0 +1,132 @@
+
+function locomotiveAnime(){
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy(".main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+});
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+locomotiveAnime()
+
+function navbarAnime(){
+    gsap.to("#navleft1 h1", {
+        transform: "translateY(-150%)",
+        scrollTrigger: {
+            trigger: "#page1",
+            scroller: ".main",
+            start: "top 0",
+            end: "top -5%",
+            scrub: true
+        }
+    })
+    gsap.to("#nav-right #links", {
+        transform: "translateY(-150%)",
+        opacity: 0,
+        scrollTrigger: {
+            trigger: "#page1",
+            scroller: ".main",
+            start: "top 0",
+            end: "top -5%",
+            scrub: true
+        }
+    })
+}
+navbarAnime()
+
+function videoconAnime(){
+var videocon = document.querySelector("#videocontainer");
+var playbtn = document.querySelector("#play");
+
+videocon.addEventListener("mouseenter", function(){
+    gsap.to(playbtn, {
+        opacity: 1,
+        scale: 1,
+
+    })
+})
+videocon.addEventListener("mouseleave", function(){
+    gsap.to(playbtn, {
+        opacity: 0,
+        scale: 0,
+        
+    })
+})
+videocon.addEventListener("mousemove", function(dets){
+    gsap.to(playbtn, {
+        left: dets.x - 80,
+        top: dets.y - 80
+    })
+})
+}
+videoconAnime()
+
+function headingAnime(){
+    gsap.from("#page1 h1", {
+        y: 100,
+        opacity: 0,
+        delay: 0.5,
+        duration: 0.8,
+        stagger: 0.3
+    })
+    gsap.from("#page1 #videocontainer", {
+        scale: .8,
+        opacity: 0,
+        delay: 1.5,
+        duration: 0.5
+    })
+}
+headingAnime()
+
+function cursorAnime(){
+var child = document.querySelectorAll(".child");
+var cursor = document.querySelector("#cursor");
+
+document.addEventListener("mousemove", function(dets){
+    gsap.to(cursor, {
+        left: dets.x,
+        top: dets.y
+    })
+}) 
+
+child.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        gsap.to(cursor, {
+            transform: "translate(-50%, -50%) scale(1)"
+        })
+    })
+})
+
+child.forEach(function(elem){
+    elem.addEventListener("mouseleave",function(){
+        gsap.to(cursor, {
+            transform: "translate(-50%, -50%) scale(0)"
+        })
+    })
+})
+}
+cursorAnime()
